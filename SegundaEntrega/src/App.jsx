d6bd1290dcdc0f2a7ac610e03c9024cb7531867a
Manuelgarcia1 <manuelalejandrogarcia@live.com.ar>
2024-05-31 18:38:25 -0300

    footer modificado

--- SegundaEntrega/src/App.jsx
+++ SegundaEntrega/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Card1 from './components/Card1/Card1'
 import Card2 from './components/Card2/Card2'
+import Footer from './components/Footer/Footer'
 function App() {
 
   return (
     <React.Fragment>
       <Card1/>
       <Card2/>
+      <Footer/>
     </React.Fragment>
   )
 }
