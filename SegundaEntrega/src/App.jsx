a8307bd936039d88f64fd0b17d32970b02f5bc47
Manuelgarcia1 <manuelalejandrogarcia@live.com.ar>
2024-05-31 17:21:28 -0300

    componentes manuel actualizado

--- SegundaEntrega/src/App.jsx
+++ SegundaEntrega/src/App.jsx
@@ -1,12 +1,14 @@
-
-import './App.css'
+import React from 'react';
+import Card1 from './components/Card1/Card1'
+import Card2 from './components/Card2/Card2'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <React.Fragment>
+      <Card1/>
+      <Card2/>
+    </React.Fragment>
   )
 }
 
