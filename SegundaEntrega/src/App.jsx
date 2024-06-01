7e76249c3daab567330d661294589e22452ad99e
Yanina-Unrein <unrein.syanina@gmail.com>
2024-06-01 17:01:12 -0300

    formulario de contacto responsivo y footer falta responsivo

--- SegundaEntrega/src/App.jsx
+++ SegundaEntrega/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/contacto" element={<FormularioContacto />} />
       </Routes>
-     
+      <Footer />
     </Router>
   );
 }
