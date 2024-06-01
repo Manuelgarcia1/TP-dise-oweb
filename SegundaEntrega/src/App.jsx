53521ecca90d846300ecc041436a2bf5958b4b51
Yanina-Unrein <unrein.syanina@gmail.com>
2024-06-01 17:41:28 -0300

    app con router

--- SegundaEntrega/src/App.jsx
+++ SegundaEntrega/src/App.jsx
@@ -4,6 +4,7 @@ import NavbarComponent from './components/navbar/NavbarComponent';
 import Footer from './components/Footer/Footer';
 import Home from './components/Home/Home';
 import FormularioContacto from './components/contacto/contacto';
+import SobreNosotros from './components/SobreNosotros/SobreNosotros';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contacto" element={<FormularioContacto />} />
+        <Route path="/sobreNosotros" element={< SobreNosotros/>} />
       </Routes>
       <Footer />
     </Router>
