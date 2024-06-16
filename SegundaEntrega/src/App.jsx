b47d123c340a1f899590eddc9bb125e27b3ddbf3
IGNACIO JOSE ROCHA <ignac1997nacho@gmail.com>
2024-06-15 23:26:02 -0300

    agrego proteccion de rutas

--- SegundaEntrega/src/App.jsx
+++ SegundaEntrega/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import NavbarComponent from './components/navbar/NavbarComponent';
 import Footer from './components/Footer/Footer';
 import Home from './components/Home/Home';
@@ -6,21 +6,34 @@ import FormularioContacto from './components/contacto/contacto';
 import SobreNosotros from './components/SobreNosotros/SobreNosotros';
 import Login from './components/seguridad/seguridad';
 import AddAlojamiento from './components/Form/form';
-import { AuthProvider } from './components/Form/AuthContext';
+import { AuthProvider, AuthContext } from './components/Form/AuthContext';
+import { useContext } from 'react';
+import PropTypes from 'prop-types';
+
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated } = useContext(AuthContext);
+
+  return isAuthenticated ? children : <Navigate to="/seguridad" />;
+};
+
+ProtectedRoute.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
 function App() {
   return (
     <AuthProvider>
-    <Router>
-      <NavbarComponent />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contacto" element={<FormularioContacto />} />
-        <Route path="/sobreNosotros" element={< SobreNosotros/>} />
-        <Route path="/seguridad" element={< Login/>} />
-        <Route path="/addAlojamiento" element={< AddAlojamiento/>} />
-      </Routes>
-      <Footer />
-    </Router>
+      <Router>
+        <NavbarComponent />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contacto" element={<FormularioContacto />} />
+          <Route path="/sobreNosotros" element={<SobreNosotros />} />
+          <Route path="/seguridad" element={<Login />} />
+          <Route path="/addAlojamiento" element={<ProtectedRoute><AddAlojamiento /></ProtectedRoute>} />
+        </Routes>
+        <Footer />
+      </Router>
     </AuthProvider>
   );
 }
