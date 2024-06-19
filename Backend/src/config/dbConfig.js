32d0db61675c4739b3653933ba52b04ab23a9bc9
Manuelgarcia1 <manuelalejandrogarcia@live.com.ar>
2024-06-19 11:27:35 -0300

    nueva rama manuel

--- Backend/src/config/dbConfig.js
+++ Backend/src/config/dbConfig.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const connection = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'ignacio',
+  password: 'manu.kayn123',
   database: 'idw',
   waitForConnections: true,
   connectionLimit: 10,
