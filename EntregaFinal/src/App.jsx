b995a0cd4b7126beac61436ae9ae2a5fd79a2dd7
Joel Chasmann <67122039+ChasmannJoel@users.noreply.github.com>
2024-06-26 19:02:42 -0300

    chau vista detallada

--- EntregaFinal/src/App.jsx
+++ EntregaFinal/src/App.jsx
@@ -6,7 +6,6 @@ import FormularioContacto from './components/contacto/contacto';
 import SobreNosotros from './components/SobreNosotros/SobreNosotros';
 import Login from './components/seguridad/seguridad';
 import AddAlojamiento from './components/Form/form';
-import VistaDetallada from './components/vistaDetallada/VistaDetallada';
 import { AuthProvider, AuthContext } from './components/Form/AuthContext';
 import { useContext } from 'react';
 import PropTypes from 'prop-types';
@@ -31,7 +30,6 @@ function App() {
           <Route path="/contacto" element={<FormularioContacto />} />
           <Route path="/sobreNosotros" element={<SobreNosotros />} />
           <Route path="/seguridad" element={<Login />} />
-          <Route path="/detalleAlojamiento/:idAlojamiento" element={<VistaDetallada />} />
           <Route path="/addAlojamiento" element={<ProtectedRoute><AddAlojamiento /></ProtectedRoute>} />
         </Routes>
         <Footer />
